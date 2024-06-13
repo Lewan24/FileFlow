68a4473c6151e82d86b372c400a0cca38d22c66d
Artur Lewandowski <LewanMordor@protonmail.com>
2024-06-13 10:55:25 +0200

    Add version command. Add automatic increasing assembly version. Add script to autoincrement version of assemblyInfo. Refactor service

---
+++ src/FileFlow.Cli/Scripts/IncrementVersion.ps1
@@ -0,0 +1,29 @@
+﻿param (
+    [string]$AssemblyInfoPath
+)
+
+$assemblyInfoContent = Get-Content $AssemblyInfoPath
+
+# Find the InformationalVersion attribute and increment its version number
+$regex = [regex]::Escape('[assembly: AssemblyInformationalVersion("') + '([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)"' + [regex]::Escape(')]')
+$matches = [regex]::Matches($assemblyInfoContent, $regex)
+
+if ($matches.Count -eq 1) {
+    $major = [int]$matches[0].Groups[1].Value
+    $minor = [int]$matches[0].Groups[2].Value
+    $build = [int]$matches[0].Groups[3].Value
+    $revision = [int]$matches[0].Groups[4].Value
+
+    # Increment the revision number
+    $revision += 1
+
+    if ($revision -ge 10) {
+        $revision = 0
+        $build += 1
+    }
+
+    $newVersion = "$major.$minor.$build.$revision"
+    $newAssemblyInfoContent = [regex]::Replace($assemblyInfoContent, $regex, "[assembly: AssemblyInformationalVersion(`"$newVersion`")]")
+
+    Set-Content $AssemblyInfoPath $newAssemblyInfoContent
+}
---
+++ src/Modules/FileFlow.Application/CodeExtensions/DirectoryExtended.cs
@@ -0,0 +1,45 @@
+﻿namespace FileFlow.Application.CodeExtensions;
+
+internal static class DirectoryExtented
+{
+    public static void Copy(string sourceDirectory, string targetDirectory)
+    {
+        DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+        DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+
+        CopyAll(diSource, diTarget);
+    }
+
+    private static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+    {
+        Directory.CreateDirectory(target.FullName);
+
+        CopyEachFileIntoNewDirectory(source, target);
+        CopyEachSubDirectoryUsingRecursion(source, target);
+    }
+
+    private static void CopyEachFileIntoNewDirectory(DirectoryInfo source, DirectoryInfo target)
+    {
+        foreach (FileInfo fi in source.GetFiles())
+        {
+            if (fi.Name == ".FileFlow")
+                continue;
+            
+            //Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+            fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+        }
+    }
+
+    private static void CopyEachSubDirectoryUsingRecursion(DirectoryInfo source, DirectoryInfo target)
+    {
+        foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+        {
+            if (diSourceSubDir.Name == ".FileFlow")
+                continue;
+
+            DirectoryInfo nextTargetSubDir =
+                target.CreateSubdirectory(diSourceSubDir.Name);
+            CopyAll(diSourceSubDir, nextTargetSubDir);
+        }
+    }
+}
